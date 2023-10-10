82e524b3a318cded8217d5fe8815854cc01c104c
BohdanBabiak1 <115339164+BohdanBabiak1@users.noreply.github.com>
2023-10-10 23:10:29 +0300

    Update script.js

--- LR1/6/script.js
+++ LR1/6/script.js
@@ -1,10 +1,19 @@
-const studentNames = []
+let studentNames = []
+let i = 1
+
 while (true) {
-    const fullName = prompt(`Введіть прізвище та ім'я студента:`)
-    if (fullName === null) {
+    studentName = prompt(`Введіть прізвище та ім'я ${i}-го студента: `)
+    if (studentName === null) {
         break;
     }
-    studentNames.push(fullName);
+
+    if (/^\p{Lu}\p{L}*\s\p{Lu}\p{L}*$/u.test(studentName)){
+        studentNames.push(studentName);
+        i += 1
+    }
+    else{
+        alert("Введіть правильне прізвище та ім'я (два слова, кожне з великої літери, розділені пробілом).")
+    }
 }
 
 studentNames.forEach(element => {
