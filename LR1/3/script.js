91b7ac4fa2221c05c688ae381f2e5adcaa34a953
BohdanBabiak1 <115339164+BohdanBabiak1@users.noreply.github.com>
2023-10-10 19:14:26 +0300

    Update script.js

--- LR1/3/script.js
+++ LR1/3/script.js
@@ -1,13 +1,19 @@
 var month = prompt("Введіть цифру місяця: ");
 
-if (month >= 1 && month <= 2 || month === 12) {
-  alert("Зима");
-} else if (month >= 3 && month <= 5) {
-  alert("Весна");
-} else if (month >= 6 && month <= 8) {
-  alert("Літо");
-} else if (month >= 9 && month <= 11) {
-  alert("Осінь");
-} else {
-  alert("Неравильне число місяця. Введіть число від 1 до 12.");
+switch (true) {
+  case (month >= 1 && month <= 2 || month === 12):
+    alert("Зима");
+    break;
+  case (month >= 3 && month <= 5):
+    alert("Весна");
+    break;
+  case (month >= 6 && month <= 8):
+    alert("Літо");
+    break;
+  case (month >= 9 && month <= 11):
+    alert("Осінь");
+    break;
+  default:
+    alert("Неправильний номер місяця. Введіть число від 1 до 12.");
+    break;
 }
