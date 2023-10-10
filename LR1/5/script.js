916f4b9def552a87ec20f379647d33da73475457
BohdanBabiak1 <115339164+BohdanBabiak1@users.noreply.github.com>
2023-10-10 20:13:40 +0300

    Update script.js

--- LR1/5/script.js
+++ LR1/5/script.js
@@ -1,8 +1,39 @@
 let names = []
-let name = prompt("Введіть кількість студентів в групі: ")
-for (let i=0; i<name; i++){
-    names.push(prompt(`Введіть ім'я та прізвище ${i+1}-го студента: `))
+let subnames = []
+
+while (true) {
+  count = prompt("Введіть кількість студентів в групі: ")
+
+  if (Number.isInteger(+count) && count != "") {
+    break
+  } else {
+    alert("Будь ласка, введіть ціле число.")
+  }
+}
+
+for (let i=0; i<count; i++){
+  while (true) {
+    name = (prompt(`Введіть ім'я ${i+1}-го студента: `))
+    if (/\s/.test(name) || /\d/.test(name) || /^[a-z]/.test(name) || name==''){
+      alert("Ім'я написано не коректно (містить пробіли, цифри або перша буква не є великою)")
+    }
+    else{ 
+      names.push(name)
+      break 
+    }
+  }
+    
+  while (true) {
+    subname = (prompt(`Введіть прізвище ${i+1}-го студента: `))
+    if (/\s/.test(subname) || /\d/.test(subname) || /^[a-z]/.test(subname) || subname==''){
+      alert("Прізвище написано не коректно (містить пробіли, цифри або перша буква не є великою)")
+    }
+    else{ 
+      subnames.push(subname)
+      break 
+    }
+  }
 }
 for (let i=0; i<names.length; i++){
-    document.write(names[i] + '<br>')
+    document.write(names[i] + " " + subnames[i] + "<br>")
 }
